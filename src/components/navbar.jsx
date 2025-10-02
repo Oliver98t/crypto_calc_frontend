0f42834c7331301045ed6c9c1867546d925aab16
Oliver98t <oli1998t@gmail.com>
2025-10-02 18:48:00 +0100

    Updated sign out button

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@heroui/button";
+import { useNavigate } from "react-router-dom";
 import { Link } from "@heroui/link";
 import {
     Navbar as HeroUINavbar,
@@ -6,21 +7,11 @@ import {
     NavbarItem,
 } from "@heroui/navbar";
 
-import {
-    GithubIcon,
-} from "@/components/icons";
-
 export const Navbar = () => {
+    const navigate = useNavigate();
+
     return (
         <HeroUINavbar maxWidth="xl" position="sticky" className="rounded-xl">
-            {/* Left-aligned GitHub icon */}
-            <NavbarContent className="basis-1/5" justify="start">
-                <NavbarItem>
-                    <Link isExternal  title="GitHub">
-                        <GithubIcon className="text-default-500" />
-                    </Link>
-                </NavbarItem>
-            </NavbarContent>
             {/* Centered title */}
             <NavbarContent className="flex-1 justify-center">
                 <NavbarItem>
@@ -37,8 +28,7 @@ export const Navbar = () => {
                     <Button
                         variant="flat"
                         onClick={() => {
-                            // TODO: Add sign out logic here
-                            alert("Signed out!");
+                            navigate("/");
                         }}
                     >
                         Sign Out
